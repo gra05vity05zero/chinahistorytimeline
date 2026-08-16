d8f50451c87e06eed970b287e34ace5c08cad75d
gra05vity05zero <gra05vity05zero@yahoo.co.jp>
2026-08-16 09:58:18 +0900

    Add A8.net banner ad to site footer

--- china-history-timeline/components/Footer.jsx
+++ china-history-timeline/components/Footer.jsx
@@ -8,6 +8,29 @@ export default function Footer() {
       className="flex flex-wrap items-center justify-center gap-4 px-6 py-8"
       style={{ backgroundColor: COLORS.paperDeep, borderTop: `1px solid ${COLORS.mist}`, fontSize: 12 }}
     >
+      <div style={{ width: "100%", textAlign: "center" }}>
+        <div style={{ fontSize: 10.5, color: COLORS.inkSoft, marginBottom: 6 }}>広告</div>
+        <a href="https://px.a8.net/svt/ejp?a8mat=4BA6D9+BJKLSQ+4XZI+HVNAP" rel="nofollow noreferrer sponsored" target="_blank">
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            border="0"
+            width="300"
+            height="250"
+            alt=""
+            src="https://www29.a8.net/svt/bgt?aid=260815293698&wid=002&eno=01&mid=s00000023067003003000&mc=1"
+            style={{ display: "inline-block" }}
+          />
+        </a>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          border="0"
+          width="1"
+          height="1"
+          src="https://www17.a8.net/0.gif?a8mat=4BA6D9+BJKLSQ+4XZI+HVNAP"
+          alt=""
+          style={{ display: "none" }}
+        />
+      </div>
       <Link href="/about" style={linkStyle}>このサイトについて</Link>
       <span style={{ color: COLORS.mist }}>|</span>
       <Link href="/privacy" style={linkStyle}>プライバシーポリシー</Link>
